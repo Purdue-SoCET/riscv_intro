3304f04b10b81ec152964d60484cd518099e65bb
Furkan Y. Akyildiz <fyabu@bu.edu>
2025-02-22 14:34:15 -0500

    instruction formats are declared

--- src/decoder.sv
+++ src/decoder.sv
@@ -4,9 +4,19 @@ module decoder
     input logic rstn_i,
     input logic enable_i,
     input logic [31:0] mem_data_i,
-    output logic 
+    output logic [4:0] rd_o,
+    output logic [4:0] rs1_o,
+    output logic [4:0] rs2_o,
+    output logic [31:0] imm_i_o,
+    output logic [31:0] imm_s_o,
+    output logic [31:0] imm_b_o,
+    output logic [31:0] imm_u_o,
+    output logic [31:0] imm_j_o,
 ); 
-    logic instr_s [31:0];
+    logic [31:0] instr_s;
+    logic [6:0] opcode_s;
+    logic [2:0] func3_s;
+    logic [6:0] func7_s;
     // Stream operator for little endian to big endiasn
     assign instr_s = {<<8{mem_data_i}};
 endmodule
